e691752b6e071eeacad8818d584c8fa403702126
unknown <daria.lantyugova@mail.ru>
2023-04-06 01:11:38 +0300

    extra task 2nd version

--- extra_task/script.js
+++ extra_task/script.js
@@ -1,8 +1,9 @@
-// функция удаляющая теги
+// функция удаляющая лишние теги и пробелы
+
 function deleteTegs(str) {
  var tags = /( |<([^>]+)>)/ig,
-     strNew = str.replace(tags, " ");
+     strNew = str.replace(tags, " ").replace(/^ +| +$|( ) +/g,"$1");
      return strNew;
 }
 
-console.log(deleteTegs('<div id="tera">Удалить лишние  <a href="link">теги</a></div> из HTML.'  ));
+console.log(deleteTegs('<div id="tera">Удалить лишние  <a href="link">теги</a></div> из HTML.'  ))
